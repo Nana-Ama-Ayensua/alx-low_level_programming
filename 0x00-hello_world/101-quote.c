de06efabc326025af76336c66715849ce8906c47
Nana-Ama-Ayensua <ayensua4u@gmail.com>
2022-10-17 15:55:11 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main -write a c program that prints to the standard error
  *
- * Return: 1
+ * Return: 1(Failed)
 */
 int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
