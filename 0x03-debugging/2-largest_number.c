b39174e5ee6cf7f3006efd2f739ea750ee05916e
Nana-Ama-Ayensua <ayensua4u@gmail.com>
2022-10-21 18:30:46 -0700

    Task 3

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ *largest_number - reeturns the largest of 3 number
+ * @a: first integer
+ * @b:second integer
+ * @c:third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	largest = a;
+
+	if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	if (c >= a && c >= b)
+	{
+		largest = c;
+	}
+	return (largest);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+
+int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
+void print_remaining_days();
+int convert_day(int month, int day);
+#endif
