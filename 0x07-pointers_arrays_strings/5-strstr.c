1b97cbc9cc077f042b95fe0456b8cf6eccedab5d
Nana-Ama-Ayensua <ayensua4u@gmail.com>
2022-11-01 17:16:04 -0700

    Task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * strstr - first occurrence of the substring needle in the string haystack
+ *_strstr - first occurrence of the substring needle in the string haystack
  * @haystack: main str to be examined
  * @needle: searched in haystack
  * Return: return 0
  **/
 
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
 {
 	char *str1, *str2; /*Declaring variables*/
 
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,8 +2,8 @@
 
 /**
  * print_chessboard - print the chessboard, 2d array
- *  @a: 2d array of chars
- *  Return: void
+ * @a: 2d array of chars
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
